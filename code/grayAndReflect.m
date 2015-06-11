ffb7f3170859864eee8ee92b3545ed5b5a02a509
Yuri <yuri@tsf-428-wpa-0-117.epfl.ch>
2015-06-11 07:40:10 +0200

    kevin microphone data

--- code/grayAndReflect.m
+++ code/grayAndReflect.m
@@ -1,8 +1,17 @@
-for i=1:25
-    I=imread(['Image' num2str(i) '.jpg']);
+% for i=1:25
+%     I=imread(['Image' num2str(i) '.jpg']);
+%     Igray=rgb2gray(I);
+%     [M,N]= size(Igray);
+%     X=reflection(Igray,N);
+%     imwrite(X,['Image' num2str(i) '.tif']);
+%     
+% end 
+
+for i=1:3
+    I=imread(['Frame' num2str(i) '.jpg']);
     Igray=rgb2gray(I);
     [M,N]= size(Igray);
     X=reflection(Igray,N);
-    imwrite(X,['Image' num2str(i) '.tif']);
+    imwrite(X,['Frame' num2str(i) '.tif']);
     
 end 
